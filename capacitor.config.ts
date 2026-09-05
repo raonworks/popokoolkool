b5fa7930214e2190fa5a43f969eadae95dc47545
devhong <popodaddy@gmail.com>
2026-09-05 23:16:36 +0900

    안드로이드 기기에서 react 웹서버 접근 허용처리

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.raonworks.apps',
-  appName: 'popokoolkool',
-  webDir: 'dist'
+  appId: "com.raonworks.apps",
+  appName: "popokoolkool",
+  webDir: "dist",
+  server: {
+    url: "http://172.16.0.130:5173/",
+    cleartext: true, //HTTP 서버 접속 허용
+    androidScheme: "https",
+  },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, //외부 접속 허용
+  },
+});
